c250ba70295bf33157b59d3f3b5b7d1b2393cb81
Juan Rodriguez (yonailo) <juanfrancisco.rodriguezhervella@atos.net>
2023-12-14 12:35:37 +0100

    Day 4

---
+++ tcc_d4.lua
@@ -0,0 +1,49 @@
+
+
+function TIC()
+  t=time() / 1500
+  for x=0,239 do
+   for y=0,135 do
+   	x1=x-60+math.sin(t/20)*120
+    y1=y-34 +math.sin(t/20)*68
+    op=1
+    u=math.cos(t)*x1+math.sin(t)*y1*op
+    v=math.cos(t)*y1-math.sin(t)*x1*op
+    a1=math.cos(t*10)*1.1 
+    a2=math.cos(t*10)*1.1
+    c=math.sin(u/(20+a1))+math.sin(v/(20+a2))-8*(t/10)
+    pix(x,y,c*8)
+   end
+  end
+
+end
+
+-- <TILES>
+-- 001:eccccccccc888888caaaaaaaca888888cacccccccacc0ccccacc0ccccacc0ccc
+-- 002:ccccceee8888cceeaaaa0cee888a0ceeccca0ccc0cca0c0c0cca0c0c0cca0c0c
+-- 003:eccccccccc888888caaaaaaaca888888cacccccccacccccccacc0ccccacc0ccc
+-- 004:ccccceee8888cceeaaaa0cee888a0ceeccca0cccccca0c0c0cca0c0c0cca0c0c
+-- 017:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
+-- 018:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+-- 019:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
+-- 020:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+-- </TILES>
+
+-- <WAVES>
+-- 000:00000000ffffffff00000000ffffffff
+-- 001:0123456789abcdeffedcba9876543210
+-- 002:0123456789abcdef0123456789abcdef
+-- </WAVES>
+
+-- <SFX>
+-- 000:000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000304000000000
+-- </SFX>
+
+-- <TRACKS>
+-- 000:100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+-- </TRACKS>
+
+-- <PALETTE>
+-- 000:1a1c2c5d275db13e53ef7d57ffcd75a7f07038b76425717929366f3b5dc941a6f673eff7f4f4f494b0c2566c86333c57
+-- </PALETTE>
+
